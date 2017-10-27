3454287d5c896d1d295a5104367acc229b249c8e
SHANE MORAN - Student <G00338607@gmit.ie>
2017-10-27 19:46:33 +0100

    part 4

--- WebApp/main.go
+++ WebApp/main.go
@@ -10,11 +10,18 @@ func handlerFunc(w http.ResponseWriter, r *http.Request) {
 	//fmt.Fprint(w, "<h1>Guessing game</h1>")
 	http.ServeFile(w,r,"guess.html")
 }
+// Name as you wish
+func page2(w http.ResponseWriter, r *http.Request) {
+
+	//fmt.Fprint(w, "<h1>Guessing game</h1>")
+	http.ServeFile(w,r,"guess.html")
+}
 
 func main() {
 
 	// Localhost : port 8080
 	http.HandleFunc("/", handlerFunc)
+	http.HandleFunc("/guess", page2)
 	http.ListenAndServe(":8080", nil)
 
 }
