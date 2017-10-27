14c589baad7db2d7ccba09f3dde98d7f45c10ca0
SHANE MORAN - Student <G00338607@gmit.ie>
2017-10-27 20:00:46 +0100

    part 5

--- WebApp/main.go
+++ WebApp/main.go
@@ -3,10 +3,19 @@ package main
 import (
 	"net/http"
 	"html/template"
+	"math/rand"
+	"time"
+	"strconv"
 )
 
 type message struct{
 	Message string
+}
+
+// random numbers
+func random(min, max int) int{
+    rand.Seed( time.Now().UnixNano())
+    return rand.Intn(max - min) + min
 }
 
 // Name as you wish
@@ -20,6 +29,30 @@ func page2(w http.ResponseWriter, r *http.Request) {
 
 	//fmt.Fprint(w, "<h1>Guessing game</h1>")
 	//http.ServeFile(w,r,"guess.tmpl")
+	mrand := random(1,20)
+	//cookies
+	cookie, err := r.Cookie("target");
+	if err != nil {
+		// Create a cookie instance and set the cookie.
+		// You can delete the Expires line (and the time import) to make a session cookie.
+		cookie = &http.Cookie{
+			Name:    "target",
+			Value:   strconv.Itoa(mrand),
+			Expires: time.Now().Add(72 * time.Hour),
+		}
+		http.SetCookie(w, cookie)
+	}
+
+	// If we could read it, try to convert its value to an int.
+	guess, _:= strconv.Atoi(r.FormValue("guess"))
+	cVal, _ := strconv.Atoi(cookie.Value)
+
+	//this checks if the cookie is same as the guess
+	if cVal == guess{
+
+
+	}
+	
 
 	onetotwenty :="Guess a number between 1 and 20"
 
