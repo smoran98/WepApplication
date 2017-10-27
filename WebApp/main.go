28bb0c9a3f54dde62bb37efbdf24748d39d0d752
SHANE MORAN - Student <G00338607@gmit.ie>
2017-10-27 19:42:21 +0100

    Part 3

--- WebApp/main.go
+++ WebApp/main.go
@@ -1,14 +1,14 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 )
 
 // Name as you wish
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
 
-	fmt.Fprint(w, "<h1>Guessing game</h1>")
+	//fmt.Fprint(w, "<h1>Guessing game</h1>")
+	http.ServeFile(w,r,"guess.html")
 }
 
 func main() {
