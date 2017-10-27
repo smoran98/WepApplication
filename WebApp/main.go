1b5fb42c8b287307c21866c9e86d38c48715e235
SHANE MORAN - Student <G00338607@gmit.ie>
2017-10-27 19:52:27 +0100

    Part 4-2

--- WebApp/main.go
+++ WebApp/main.go
@@ -2,7 +2,12 @@ package main
 
 import (
 	"net/http"
+	"html/template"
 )
+
+type message struct{
+	Message string
+}
 
 // Name as you wish
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
@@ -14,11 +19,16 @@ func handlerFunc(w http.ResponseWriter, r *http.Request) {
 func page2(w http.ResponseWriter, r *http.Request) {
 
 	//fmt.Fprint(w, "<h1>Guessing game</h1>")
-	http.ServeFile(w,r,"guess.html")
+	//http.ServeFile(w,r,"guess.tmpl")
+
+	onetotwenty :="Guess a number between 1 and 20"
+
+	t, _ := template.ParseFiles("guess.tmpl")
+
+	t.Execute(w, &message{Message:onetotwenty})
 }
 
 func main() {
-
 	// Localhost : port 8080
 	http.HandleFunc("/", handlerFunc)
 	http.HandleFunc("/guess", page2)
